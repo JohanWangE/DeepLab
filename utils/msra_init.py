21a36a6d245d9b34940462ea40a92350ba20b1d2
2prime <luyiping9712@pku.edu.cn>
2017-07-17 12:10:18 +0800

    Add files via upload

---
+++ utils/msra_init.py
@@ -0,0 +1,45 @@
+'''
+MSRA INIT
+net parameter initialization.
+He K, Zhang X, Ren S, et al.
+Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classification,
+arxiv:1502.01852
+'''
+
+import os
+import sys
+import time
+import math
+
+import torch.nn as nn
+import torch.nn.init as init
+
+
+'''
+useage:
+class Net(nn.Module):
+    def __init__(*parameters):
+        super(Net, self).__init__()
+        ...parameter setting
+
+        msra_init(self)
+
+    def forward(*para):
+        ...
+'''
+
+
+def msra_init(net):
+    '''Init layer parameters.'''
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            init.kaiming_normal(m.weight, mode='fan_out')
+            if m.bias:
+                init.constant(m.bias, 0)
+        elif isinstance(m, nn.BatchNorm2d):
+            init.constant(m.weight, 1)
+            init.constant(m.bias, 0)
+        elif isinstance(m, nn.Linear):
+            init.normal(m.weight, std=1e-3)
+            if m.bias:
+                init.constant(m.bias, 0)
