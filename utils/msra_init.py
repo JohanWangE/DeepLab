76dd812fc0779731d7c8e2bf8cc4379aeb59c71d
2prime <luyiping9712@pku.edu.cn>
2017-07-18 22:09:51 +0800

    Update msra_init.py

--- utils/msra_init.py
+++ utils/msra_init.py
@@ -28,18 +28,17 @@ class Net(nn.Module):
         ...
 '''
 
-
 def msra_init(net):
     '''Init layer parameters.'''
     for m in net.modules():
         if isinstance(m, nn.Conv2d):
-            init.kaiming_normal(m.weight, mode='fan_out')
+            init.kaiming_normal(m.weight)
             if m.bias:
                 init.constant(m.bias, 0)
         elif isinstance(m, nn.BatchNorm2d):
             init.constant(m.weight, 1)
             init.constant(m.bias, 0)
         elif isinstance(m, nn.Linear):
-            init.normal(m.weight, std=1e-3)
+            init.kaiming_normal(m.weight)
             if m.bias:
                 init.constant(m.bias, 0)
