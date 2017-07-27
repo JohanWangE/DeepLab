2a2ce3ef8dc636b8ff22c6a985f765fd3339de1b
2prime <luyiping9712@pku.edu.cn>
2017-07-28 06:56:20 +0800

    Add files via upload
    Add reflect padding for pytorch while doing data augmentation

---
+++ utils/padding.py
@@ -0,0 +1,99 @@
+from PIL import Image
+from PIL._util import isStringType
+import operator
+import numbers
+import functools
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+
+def _border(border):
+    if isinstance(border, tuple):
+        if len(border) == 2:
+            left, top = right, bottom = border
+        elif len(border) == 4:
+            left, top, right, bottom = border
+    else:
+        left = top = right = bottom = border
+    return left, top, right, bottom
+
+def _color(color, mode):
+    if isStringType(color):
+        from PIL import ImageColor
+        color = ImageColor.getcolor(color, mode)
+    return color
+
+def expand(image, border=0):
+    """
+    Add border to the image(Symmetric padding)
+
+    :param image: The image to expand.
+    :param border: Border width, in pixels.
+    :return: An image.
+    """
+    img = np.asarray(image)
+    img = np.pad(img,pad_width=border,mode="reflect")
+    return Image.fromarray(np.uint8(img[:,:,2:5]))
+
+class Reflect_Pad(object):
+    """Pads the given PIL.Image on all sides with the given "pad" """
+
+    def __init__(self, padding):
+        assert isinstance(padding, numbers.Number)
+        self.padding = padding
+
+    def __call__(self, img):
+        return expand(img, border=self.padding)
+
+
+def imshow(img):
+    img = img / 2 + 0.5 # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1,2,0)))
+    plt.show()
+
+
+if __name__ == "__main__":
+    import torch
+    import torchvision
+    import  torchvision.transforms as transforms
+
+    global_batch_size = 10
+
+    transform_train = transforms.Compose([
+        Reflect_Pad(2),
+        #transforms.RandomCrop(32, padding=0),
+        #transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    # Here per-pixel mean isn't subtracted
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root="./data", download=True, train=True, transform=transform_train
+    )
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=global_batch_size, shuffle=True, num_workers=2
+    )
+
+    testset = torchvision.datasets.CIFAR10(
+        root="./data", download=True, train=False, transform=transform_test
+    )
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=global_batch_size, shuffle=False, num_workers=2
+    )
+
+    # show some random training images
+    dataiter = iter(trainloader)
+    images, labels = dataiter.next()
+
+    # print images
+    imshow(torchvision.utils.make_grid(images))
