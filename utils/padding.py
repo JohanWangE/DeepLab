4bbac3a9d981e83ecbcb21fa6bdc1a34ef76d2f3
2prime <luyiping9712@pku.edu.cn>
2017-07-28 13:49:40 +0800

    Update padding.py

--- utils/padding.py
+++ utils/padding.py
@@ -30,7 +30,50 @@ class Reflect_Pad(object):
 
     def __call__(self, img):
         return expand(img, border=self.padding)
+def expand_edge(image, border=0):
+    """
+    Add border to the image(Symmetric padding)
 
+    :param image: The image to expand.
+    :param border: Border width, in pixels.
+    :return: An image.
+    """
+    img = np.asarray(image)
+    img = np.pad(img,pad_width=border,mode="edge")
+    return Image.fromarray(np.uint8(img[:,:,2:5]))
+
+class Edge_Pad(object):
+    """Pads the given PIL.Image on all sides with the given "pad":edge pad """
+
+    def __init__(self, padding):
+        assert isinstance(padding, numbers.Number)
+        self.padding = padding
+
+    def __call__(self, img):
+        return expand_edge(img, border=self.padding)
+
+
+def expand_symmetric(image, border=0):
+    """
+    Add border to the image(Symmetric padding)
+
+    :param image: The image to expand.
+    :param border: Border width, in pixels.
+    :return: An image.
+    """
+    img = np.asarray(image)
+    img = np.pad(img,pad_width=border,mode="symmetric")
+    return Image.fromarray(np.uint8(img[:,:,2:5]))
+
+class Symmetric_Pad(object):
+    """Pads the given PIL.Image on all sides with the given "pad":symmetric pad """
+
+    def __init__(self, padding):
+        assert isinstance(padding, numbers.Number)
+        self.padding = padding
+
+    def __call__(self, img):
+        return expand_symmetric(img, border=self.padding)
 
 def imshow(img):
     img = img / 2 + 0.5 # unnormalize
